994d9f9173e48870a4f72faf11d7eedd9f6eb960
Grandlear <grandlear1@yahoo.com>
2015-08-02 19:17:07 -0400

    JavaScript Cleanup
    Encapsulating entire file into self-executing function.
    Moving each shop into their own object.
    Created new functions for each shop to handle calculation of earnings.
    Refactored on...Clicked() events to fix bugs: 1) shops could be bought
    without having appropriate funds; 2) shops automatically upgraded after
    first purchase

--- idle.js
+++ idle.js
@@ -1,112 +1,113 @@
-var score = 0;
-var btnIncrement = 1;
-var btnTotalClicks = 0;
-
-var coffeeShopPrice = 10;
-var coffeeShopValue = 1;
-var coffeeShopMP = 2;
-var isCoffeeShopBought = false;
-
-var cardShopPrice = 100;
-var cardShopValue = 10;
-var cardShopMP = 4;
-var isCardShopBought = false;
-
-var bowlingAlleyPrice = 1000;
-var bowlingAlleyValue = 250;
-var bowlingAlleyMP = 8;
-var bowlingAlleyBought = false;
-
-function btnClick () {
-    btnTotalClicks++;
-    addToScore(btnIncrement);
-    if (btnTotalClicks % 10 == 0) btnIncrement = btnIncrement * 2;
-}
-
-function onManualEarnClicked() {
-    btnClick();
-}
-
-function addToScore (val) {
-    score = score + val;
+(function () {
+    'use strict';
+    var score = 0;
+    var btnIncrement = 1;
+    var btnTotalClicks = 0;
+    
+    var coffeeShop = {
+        price : 10,
+        value : 1,
+        multiplier : 2,
+        isBought : false
+    };
+    
+    var cardShop = {
+        price : 100,
+        value : 10,
+        multiplier : 4,
+        isBought : false
+    };
+    
+    var bowlingAlley = {
+        price : 1000,
+        value : 250,
+        multiplier : 8,
+        isBought : false
+    }
+    
+    function btnClick () {
+        btnTotalClicks++;
+        addToScore(btnIncrement);
+        if (btnTotalClicks % 10 === 0) btnIncrement = btnIncrement * 2;
+    }
+    
+    function onManualEarnClicked() {
+        btnClick();
+    }
+    
+    function addToScore (val) {
+        score = score + val;
+        refreshData();
+    }
+    
+    function refreshData() {
+        document.getElementById('score').innerHTML = score;
+        document.getElementById('coffee-shop-value').innerHTML = coffeeShop.value;
+        document.getElementById('coffee-shop-next-upgrade').innerHTML = coffeeShop.price;
+        document.getElementById('card-shop-value').innerHTML = cardShop.value;
+        document.getElementById('card-shop-next-upgrade').innerHTML = cardShop.price;
+        document.getElementById('bowling-alley-value').innerHTML = bowlingAlley.value;
+        document.getElementById('bowling-alley-next-upgrade').innerHTML = bowlingAlley.price;
+    }
+    
+    function increaseEarnings (val) {
+        addToScore(val);
+    }
+    
+    function calculateCoffeeShopEarnings () {
+        addToScore(coffeeShop.value);
+    }
+    
+    function onCoffeeShopClicked() {
+        if (score >= coffeeShop.price) {
+            score = score - coffeeShop.price;
+            var newValue = coffeeShop.value;
+            newValue = coffeeShop.multiplier * coffeeShop.value;
+            coffeeShop.value = newValue;
+            coffeeShop.price = (coffeeShop.price * 2) * coffeeShop.multiplier;
+            coffeeShop.isBought = true;
+            setInterval(calculateCoffeeShopEarnings, 50);
+        }
+        
+    }
+    
+    function calculateCardShopEarnings () {
+        addToScore(cardShop.value);
+    }
+    
+    function onCardShopClicked() {
+        if (score >= cardShop.price) {
+            score = score - cardShop.price;
+            var newValue = cardShop.value;
+            newValue = cardShop.multiplier * cardShop.value;
+            cardShop.value = newValue;
+            cardShop.price = (cardShop.price * 2) * cardShop.multiplier;
+            cardShop.isBought = true;
+            setInterval(calculateCardShopEarnings, 50);
+        }
+    }
+    
+    
+    function calculateBowlingAlleyEarnings () {
+        addToScore(bowlingAlley.value);
+    }
+    
+    function onBowlingAlleyClicked() {
+        if (score >= bowlingAlley.price) {
+            score = score - bowlingAlley.price;
+            var newValue = bowlingAlley.value;
+            newValue = bowlingAlley.multiplier * bowlingAlley.value;
+            bowlingAlley.value = newValue;
+            bowlingAlley.price = (bowlingAlley.price * 2) * bowlingAlley.multiplier;
+            bowlingAlley.isBought = true;
+            setInterval(calculateBowlingAlleyEarnings, 50);
+        }
+    }
+    
     refreshData();
-}
-
-function refreshData() {
-    document.getElementById('score').innerHTML = score;
-    document.getElementById('coffee-shop-value').innerHTML = coffeeShopValue;
-    document.getElementById('coffee-shop-next-upgrade').innerHTML = coffeeShopPrice;
-    document.getElementById('card-shop-value').innerHTML = cardShopValue;
-    document.getElementById('card-shop-next-upgrade').innerHTML = cardShopPrice;
-    document.getElementById('bowling-alley-value').innerHTML = bowlingAlleyValue;
-    document.getElementById('bowling-alley-next-upgrade').innerHTML = bowlingAlleyPrice;
-}
-
-function increaseEarnings (val) {
-    addToScore(val);
-}
-
-function coffeeShop () {
-    var newValue = coffeeShopValue;
-    if (score >= coffeeShopPrice) {
-        score = score - coffeeShopPrice;
-        newValue = coffeeShopMP * coffeeShopValue;
-        coffeeShopValue = newValue;
-        coffeeShopPrice = (coffeeShopPrice * 2) * coffeeShopMP;
-        iscoffeeShopBought = true;
-    }
     
-    if (iscoffeeShopBought = true) {
-        addToScore(newValue);
-    }
-}
-
-function onCoffeeShopClicked() {
-    setInterval(coffeeShop, 50);
-}
-
-function cardShop () {
-    var newValue = cardShopValue;
-    if (score >= cardShopPrice) {
-        score = score - cardShopPrice;
-        newValue = cardShopMP * cardShopValue;
-        cardShopValue = newValue;
-        cardShopPrice = (cardShopPrice * 2) * cardShopMP;
-        iscardShopBought = true;
-    }
-  
-    if (iscardShopBought = true) {
-        addToScore(newValue);
-    }
-}
-
-function onCardShopClicked() {
-    setInterval(cardShop, 50)
-}
-
-
-function bowlingAlley () {
-    var newValue = bowlingAlleyValue;
-    if (score >= bowlingAlleyPrice) {
-        score = score - bowlingAlleyPrice;
-        newValue = bowlingAlleyMP * bowlingAlleyValue;
-        bowlingAlleyValue = newValue;
-        bowlingAlleyPrice = (bowlingAlleyPrice * 2) * bowlingAlleyMP;
-        bowlingAlleyBought = true;
-    }
-    
-    if (bowlingAlleyBought = true) {
-        addToScore(newValue);
-    }
-}
-
-function onBowlingAlleyClicked() {
-    setInterval(bowlingAlley, 50);
-}
-
-refreshData();
-
-document.getElementById('manual-earn').onclick = onManualEarnClicked;
-document.getElementById('coffee-shop').onclick = onCoffeeShopClicked;
-document.getElementById('card-shop').onclick = onCardShopClicked;
-document.getElementById('bowling-alley').onclick = onBowlingAlleyClicked;
+    document.getElementById('manual-earn').onclick = onManualEarnClicked;
+    document.getElementById('coffee-shop').onclick = onCoffeeShopClicked;
+    document.getElementById('card-shop').onclick = onCardShopClicked;
+    document.getElementById('bowling-alley').onclick = onBowlingAlleyClicked;
+})();
